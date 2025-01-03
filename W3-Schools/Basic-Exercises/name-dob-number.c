67ffa51a186c4c6977105905102f2d5b5e98c325
foxtw00 <suneyh@protonmail.com>
2025-01-03 10:05:01 +0300

    Create name-dob-number.c
    C Basic 1 - W3 Schools Learning Path

---
+++ W3-Schools/Basic-Exercises/name-dob-number.c
@@ -0,0 +1,11 @@
+// C Basic 1 - W3 Schools Learning Path
+// Written by foxtw00
+
+#include <stdlib.h>
+
+int main(void){
+    printf("Name   : foxtw00\n");
+    printf("DOB    : 1-1-1990\n");
+    printf("Mobile : 999-999-9999\n");
+    return 0;
+}
