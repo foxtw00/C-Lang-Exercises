a723831cb3bb79e78b458883b33f7e0ac25228b2
foxtw00 <suneyh@protonmail.com>
2025-01-03 10:09:24 +0300

    Create reverse-print.c
    C Basic 4 - W3 Schools Learning Path

---
+++ W3-Schools/Basic-Exercises/reverse-print.c
@@ -0,0 +1,18 @@
+// C Basic 4 - W3 Schools Learning Path
+// Written by foxtw00
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    char one,two,three;
+    one = 'X';
+    two = 'M';
+    three = 'L';
+
+    printf("The reverse of %c%c%c is %c%c%c\n", one, two, three, three, two, one);
+
+    return 0;
+
+}
