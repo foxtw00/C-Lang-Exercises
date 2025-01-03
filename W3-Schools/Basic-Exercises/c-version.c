9c170e6693ca0fabd3dbc7c6a1b7afa68a4c78be
foxtw00 <suneyh@protonmail.com>
2025-01-03 10:07:02 +0300

    Create c-version.c
    C Basic 2 - W3 Schools Learning Path

---
+++ W3-Schools/Basic-Exercises/c-version.c
@@ -0,0 +1,10 @@
+// C Basic 2 - W3 Schools Learning Path
+// Written by foxtw00
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("The C Version is %ld\n", __STDC_VERSION__);
+    return 0;
+}
