5f932ac20456dd21ae467bd5173d23e280504775
foxtw00 <suneyh@protonmail.com>
2025-01-03 10:08:05 +0300

    Create newline-letters.c
    C Basic 3 - W3 Schools Learning Path

---
+++ W3-Schools/Basic-Exercises/newline-letters.c
@@ -0,0 +1,10 @@
+// C Basic 3 - W3 Schools Learning Path
+// Written by foxtw00
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    printf("######\n#\n#\n#####\n#\n#\n#\n  ######\n ##      ##\n#\n#\n#\n#\n#\n ##      ##\n   ######\n");
+    return 0;
+}
